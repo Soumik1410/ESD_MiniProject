ef15bd7d25d6d2963ce6a66269f7274c154b62a5
soumik <soumik1410code@gmail.com>
2024-11-27 23:34:56 +0530

    Revert "1. Added SecurityConfig.java for password encoder method"
    This reverts commit 653d337465ae8d27fd7d1b5cd978d7f6990b5f87.

--- esd_project_backend/src/main/java/org/example/esd_project_backend/EsdProjectBackendApplication.java
+++ esd_project_backend/src/main/java/org/example/esd_project_backend/EsdProjectBackendApplication.java
@@ -2,9 +2,8 @@ package org.example.esd_project_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class EsdProjectBackendApplication {
 
 	public static void main(String[] args) {
