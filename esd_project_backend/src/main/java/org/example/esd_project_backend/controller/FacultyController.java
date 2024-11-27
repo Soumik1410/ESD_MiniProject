54fe9c09ef6c42e617e1e740c087f71cf2dd92a1
soumik <soumik1410code@gmail.com>
2024-11-27 23:56:27 +0530

    Added Annotation at Controller level on FacultyController.java to allow cross origin requests from http://localhost:3000 for all 3 endpoints

--- esd_project_backend/src/main/java/org/example/esd_project_backend/controller/FacultyController.java
+++ esd_project_backend/src/main/java/org/example/esd_project_backend/controller/FacultyController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/faculty")
